64bd15eb069e0144134de5d182c4dca9c6174384
Esubale-Endale <esu1011esu@gmail.com>
2026-09-18 19:09:09 +0300

    feat: add workspace configuration and update main entry point to MiniMarketApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_demo/Counter/app.dart';
 
-// import 'package:flutter_demo/MiniMarket/app.dart';
+import 'package:flutter_demo/MiniMarket/app.dart';
 
 void main() {
-  runApp(const CounterApp());
+  runApp(const MiniMarketApp());
 }
